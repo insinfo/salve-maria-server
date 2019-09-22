8852cce2d754510132cb80ad4c45dbc2ef9bf341
isaque neves <insinfo2008@gmail.com>
2019-09-21 21:04:17 -0300

    to slim

---
+++ web/cache/af/afd8ff00cf1413b340aada177058282097c062dbbbd401afa325e180c2accd93.php
@@ -0,0 +1,62 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* home.html */
+class __TwigTemplate_dbafd371b2aa3d91709575b622abee1c1e120d62f6c2a7495afe87224697f7e8 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+
+</body>
+</html>";
+    }
+
+    public function getTemplateName()
+    {
+        return "home.html";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "home.html", "C:\\heroku\\salve-maria-server\\web\\views\\home.html");
+    }
+}
--- web/index.php
+++ web/index.php
@@ -1,25 +1,121 @@
 <?php
 
-require('../vendor/autoload.php');
+$BASE_DIR = dirname(__FILE__);
+$VIEWS_DIR = $BASE_DIR.'/views';
+$CACHE_DIR = $BASE_DIR.'/cache';
 
-$app = new Silex\Application();
-$app['debug'] = true;
+//require '../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
 
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
+use DI\Container;
+use Slim\Views\Twig;
+use Slim\Views\TwigExtension;
+//use Slim\Views\TwigMiddleware;
 
-// Our web handlers
 
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Slim\Interfaces\RouteParserInterface;
+class TwigMiddleware implements MiddlewareInterface
+{
+    /**
+     * @var Twig
+     */
+    protected $twig;
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+    /**
+     * @var RouteParserInterface
+     */
+    protected $routeParser;
+    /**
+     * @var string
+     */
+    protected $basePath;
+    /**
+     * @param Twig                 $twig
+     * @param ContainerInterface   $container
+     * @param RouteParserInterface $routeParser
+     * @param string               $basePath
+     */
+    public function __construct(Twig $twig, ContainerInterface $container, RouteParserInterface $routeParser, string $basePath = '')
+    {
+        $this->twig = $twig;
+        $this->container = $container;
+        $this->routeParser = $routeParser;
+        $this->basePath = $basePath;
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+      //$basePath = rtrim(str_ireplace('index.php', '', $request->getUri()->getBasePath()), '/');
+
+        $extension = new TwigExtension($this->routeParser, $request->getUri(), $this->basePath);
+        $this->twig->addExtension($extension);
+        if (method_exists($this->container, 'set')) {
+            $this->container->set('view', $this->twig);
+        } elseif ($this->container instanceof ArrayAccess) {
+            $this->container['view'] = $this->twig;
+        }
+        return $handler->handle($request);
+    }
+}
+
+// Create Container
+$container = new Container();
+AppFactory::setContainer($container);
+
+$app = AppFactory::create();
+
+// Add Routing Middleware
+$app->addRoutingMiddleware();
+
+/*
+ * Add Error Handling Middleware
+ *
+ * @param bool $displayErrorDetails -> Should be set to false in production
+ * @param bool $logErrors -> Parameter is passed to the default ErrorHandler
+ * @param bool $logErrorDetails -> Display error details in error log
+ * which can be replaced by a callable of your choice.
+ 
+ * Note: This middleware should be added last. It will not handle any exceptions/errors
+ * for middleware added after it.
+ */
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
+// Add Twig-View Middleware
+
+$routeParser = $app->getRouteCollector()->getRouteParser();
+$twig = new Twig($VIEWS_DIR, ['cache' => false]/*['cache' => $CACHE_DIR]*/);
+$twigMiddleware = new TwigMiddleware($twig, $app->getContainer(), $routeParser);
+$app->add($twigMiddleware);
+
+
+$app->get('/', function (Request $request, Response $response, array $args) {
+ 
+  //$response->getBody()->write("Hello");
+  // return $response;
+    return $this->get('view')->render($response, 'home.html');
+ 
+});
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+    return $response;
 });
 
 $app->run();
+
+
